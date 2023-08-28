e8ba6f2a0ceac9fb558ce7038b3f6bafa75c09b4
Ferreiramaria <136329026+Ferreiramaria@users.noreply.github.com>
2023-08-28 12:02:52 -0300

    BLABLABLA

---
+++ FlappyRio/Assets/Script/playerScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    
+    public int forçaPulo;
+    Rigidbody2D rbPlayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody2D>();
+        rbPlayer.AddForce(new Vector2(0, forçaPulo), ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rbPlayer.velocity = Vector2.zero;
+            rbPlayer.AddForce(new Vector2(0, forçaPulo), ForceMode2D.Impulse);
+            
+        }
+    }
+}
