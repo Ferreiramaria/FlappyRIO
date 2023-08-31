2a87a403ad994b878973e559fc511620e7d52ab8
Ferreiramaria <136329026+Ferreiramaria@users.noreply.github.com>
2023-08-31 17:07:36 -0300

    BLABLABLA

---
+++ FlappyRio/Assets/Parallax.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float speed;
+    public float limiteX;
+    public float posInicioX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x <= limiteX)
+        {
+            transform.position = new Vector2(posInicioX, transform.position.y);
+        }
+
+        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+    }
+}
--- FlappyRio/Assets/Script/obstaculo.cs
+++ FlappyRio/Assets/Script/obstaculo.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class obstaculo : MonoBehaviour
 {
     public int mov;
+    
 
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x - mov *Time.deltaTime, transform.position.y, transform.position.z);
-        
+
+        transform.position = new Vector3(transform.position.x - mov * Time.deltaTime, transform.position.y, transform.position.z);
     }
+    
+
 }
