b73b70bd9c72718b6cf45939a93200e96188a530
Ferreiramaria <136329026+Ferreiramaria@users.noreply.github.com>
2023-09-04 12:12:30 -0300

    NOVAS

--- FlappyRio/Assets/Script/GameManager.cs
+++ FlappyRio/Assets/Script/GameManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject obstaculo;
+    private float intervalo;
+    public float intervaloParaAd;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        intervalo = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (intervalo <= Time.time)
+        {
+            Instantiate(obstaculo, new Vector3(6, Random.Range(-1.31f, 2), 0), Quaternion.identity);
+            intervalo = Time.time + intervaloParaAd;
+        }
+       
     }
 }
