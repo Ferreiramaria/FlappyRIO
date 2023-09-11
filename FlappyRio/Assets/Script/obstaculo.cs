42fed5a67387bd1025f44820e02d8cba2ebb5315
Ferreiramaria <136329026+Ferreiramaria@users.noreply.github.com>
2023-09-11 20:54:26 -0300

    Atualização1

--- FlappyRio/Assets/Script/obstaculo.cs
+++ FlappyRio/Assets/Script/obstaculo.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class obstaculo : MonoBehaviour
 {
     public int mov;
+    public float verticalSpeed = 0.5f;
+
 
 
 
@@ -17,6 +19,19 @@ public class obstaculo : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(transform.position.x - mov * Time.deltaTime, transform.position.y, transform.position.z);
+        
+        float newY = transform.position.y + verticalSpeed * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+        if (newY >= 2.23f)
+        {
+            verticalSpeed = -Mathf.Abs(verticalSpeed); // Inverte a direção vertical
+        }
+
+        if (newY <= -2f)
+        {
+            verticalSpeed = Mathf.Abs(verticalSpeed); // Inverte a direção vertical
+        }
 
         if (transform.position.x <= -5)
         {
